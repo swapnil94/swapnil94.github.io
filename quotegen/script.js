f85c261be21afa51fe489018baba355f09ea0b4e
Swapnil <swapnil.bittu@gmail.com>
2016-07-06 14:06:44 +0530

    http removed

--- quotegen/script.js
+++ quotegen/script.js
@@ -3,7 +3,7 @@ $("document").ready(function() {
   $("#generate").on("click",newQuote);
 });
 function newQuote(){
-	$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(data) {
+	$.getJSON("api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(data) {
         console.log(data);
 	});
 }
