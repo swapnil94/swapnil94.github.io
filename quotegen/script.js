9f0fd92961e52ff396c81c283e6c727b8ebb2e6e
Swapnil <swapnil.bittu@gmail.com>
2016-07-09 21:00:39 +0530

    9/7/2016

--- quotegen/script.js
+++ quotegen/script.js
@@ -3,7 +3,7 @@ $("document").ready(function() {
   $("#generate").on("click",newQuote);
 });
 function newQuote(){
-	$.getJSON("api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(data) {
+	$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", function(data) {
         console.log(data);
 	});
 }
