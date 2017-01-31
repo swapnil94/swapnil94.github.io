f3fc9c0a1cb33e89a1340adf973099e3e173092d
Swapnil <Swapnil>
2017-01-31 16:48:27 +0530

    update 31/1/17 4PM

--- weather/script.js
+++ weather/script.js
@@ -1,107 +1,157 @@
-var unit="metric",temp;
+
+const unit="metric";
+const weatherAPIKey = "5666c4c14907952edf5f47bf61b14bd9";
+const geocodeAPIKey = "AIzaSyDpJsYiCtBTAo5x5-ABdGlx38lrhSOgR4M";
+
 $(document).ready(function() {
-  $("#changeunit").on("click", function() {
-    if (unit == "metric") {
+  
+  /*$("#changeunit").on("click", function() {
+    
+    if(unit == "metric") {
       unit = "imperial";
       temp = (temp * 1.8 + 32).toFixed(1);
       $("#temp").html(temp + "°F");
-    } else if (unit == "imperial") {
+    }else if(unit == "imperial") {
       unit = "metric";
       temp = ((temp - 32) / 1.8).toFixed(1);
       $("#temp").html(temp + "°C");
     }
-  });
-  $("#locsubmit").on("click",getweather($("#locinput").html));
+  });*/
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        let latlonString = position.coords.latitude + "," + position.coords.longitude;
+        fromLatLon(latlonString);
+      },
+      showError,
+      {enableHighAccuracy:false, timeout:5000, maximumAge:0}
+    );
+  }
 });
-function getweather(){
-  var skycons = new Skycons();
-  console.log("started");
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $.getJSON("https://api.darksky.net/forecast/5666c4c14907952edf5f47bf61b14bd9/" + position.coords.latitude + "," + position.coords.longitude + "?units=si&callback=?", function(json) {
-        temp = json.currently.temperature.toFixed(1);
-        var icon = json.currently.icon;
-        var loc = "testloc";//json.name + ',' + json.sys.country;
-        var weather = json.hourly.summary; 
-        var wind = "";
-        var winddeg = json.currently.windBearing;
-        if (winddeg == 90)
-          wind += "E";
-        else if (winddeg == 180)
-          wind += "S";
-        else if (winddeg == 270)
-          wind += "W";
-        else if (winddeg == 360)
-          wind += "N";
-        else if (winddeg < 90)
-          wind += "NE";
-        else if (winddeg < 180)
-          wind += "SE";
-        else if (winddeg < 270)
-          wind += "SW";
-        else if (winddeg < 360)
-          wind += "NW";
-        wind += " " + json.currently.windSpeed + " knots";
-        $("#location").html(loc);
-        $("#weather").html(weather);
-        $("#wind").html(wind);
-        $("#temp").html(temp + "°C");
-        skycons.add("wicon",icon);
-		skycons.play();
-        //$("#test").html(JSON.stringify(json));
-        switch (icon) {
-          case "clear-day":
-            $("body").css("background", "url(images/clear_day.jpg) no-repeat");
-            break;
-          case "clear-night":
-            $("body").css("background", "url(images/clear_night.jpg) no-repeat");
-            break;
-          case "rain":
-            $("body").css("background", "url(images/rain.jpg) no-repeat");
-            break;
-          case "snow":
-            $("body").css("background", "url(images/snow.jpg) no-repeat");
-            break;
-          case "sleet":
-            $("body").css("background", "url(images/sleet.jpg) no-repeat");
-            break;
-          case "wind":
-            $("body").css("background", "url(images/wind.jpg) no-repeat");
-            break;
-          case "fog":
-            $("body").css("background", "url(images/fog.jpg) no-repeat");
-            break;
-          case "cloudy":
-            $("body").css("background", "url(images/cloudy.jpg) no-repeat");
-            break;
-          case "partly-cloudy-day":
-            $("body").css("background", "url(images/cloudy.jpg) no-repeat");
-            break;
-          case "partly-cloudy-night":
-            $("body").css("background", "url(images/cloudy_night.jpg) no-repeat");
-            break;
-          default:
-            $("body").css("background", "url(images/default.jpg) no-repeat");
-        }
-        $("body").css("background-size", "cover");
-      });
-    },function(error) {
-      //$("#location").html(error.code);
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("An unknown error occurred.");
-            break;
+
+function fromLatLon(latlonString){
+
+  let APIEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+latlonString+"&key="+geocodeAPIKey;
+  $.getJSON(
+    APIEndpoint,
+    function(json){
+      $("#location").html(json.results[2].formatted_address);
+      console.log(json.results[2].formatted_address);
+      getWeather(latlonString);
+    });
+}
+
+function fromLocation(loc){
+  if (typeof loc == 'undefined'){
+    loc = $("#locinput").val();
+  }
+  let APIEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?address="+loc+"&key="+geocodeAPIKey;
+  console.log(APIEndpoint);
+  $.getJSON(
+    APIEndpoint,
+    function(json){
+      $("#location").html(json.results[0].formatted_address);
+      let latlonString = json.results[0].geometry.location.lat + "," + json.results[0].geometry.location.lng;
+      console.log(json.results[0].formatted_address);
+      getWeather(latlonString);
+    });
+}
+
+function getWeather(latlonString){
+
+  let weatherAPIEndpoint = "https://api.darksky.net/forecast/"+ weatherAPIKey+ "/" + latlonString + "?units=ca&callback=?";
+  $.getJSON(
+    weatherAPIEndpoint,
+    function(json) {
+      let temp = json.currently.temperature.toFixed(1);
+      let weather = json.hourly.summary; 
+      let wind = getWindDirection(json.currently.windBearing, json.currently.windSpeed);
+      //$("#location").html(loc);
+      $("#weather").html(weather);
+      $("#wind").html(wind);
+      $("#temp").html(temp + " °C");
+      setSkyConWallpaper(json.currently.icon);
     }
-      
-},{ enableHighAccuracy:false,timeout:5000,maximumAge:0});
+  );
+}
+
+function showError(error){
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation. Please enter location in search box.");
+        break;
+    case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable. Please enter location in search box.");
+        break;
+    case error.TIMEOUT:
+        alert("The request to get user location timed out. Please enter location in search box.");
+        break;
+    case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred. Please try entering location in search box.");
+        break;
+    }
+}
+
+function getWindDirection(winddeg, windspeed){
+  let wind = "";
+  if (winddeg == 90)
+    wind = "E";
+  else if (winddeg == 180)
+    wind = "S";
+  else if (winddeg == 270)
+    wind = "W";
+  else if (winddeg == 360)
+    wind = "N";
+  else if (winddeg < 90)
+    wind = "NE";
+  else if (winddeg < 180)
+    wind = "SE";
+  else if (winddeg < 270)
+    wind = "SW";
+  else if (winddeg < 360)
+    wind = "NW";
+  
+  wind += " " + windspeed + " knots";
+  return wind;
+}
+
+function setSkyConWallpaper(icon){
+  let skycons = new Skycons();
+  skycons.add("wicon",icon);
+  skycons.play();
+  switch (icon) {
+    case "clear-day":
+      $("body").css("background", "url(images/clear_day.jpg) no-repeat");
+      break;
+    case "clear-night":
+      $("body").css("background", "url(images/clear_night.jpg) no-repeat");
+      break;
+    case "rain":
+      $("body").css("background", "url(images/rain.jpg) no-repeat");
+      break;
+    case "snow":
+      $("body").css("background", "url(images/snow.jpg) no-repeat");
+      break;
+    case "sleet":
+      $("body").css("background", "url(images/sleet.jpg) no-repeat");
+      break;
+    case "wind":
+      $("body").css("background", "url(images/wind.jpg) no-repeat");
+      break;
+    case "fog":
+      $("body").css("background", "url(images/fog.jpg) no-repeat");
+      break;
+    case "cloudy":
+      $("body").css("background", "url(images/cloudy.jpg) no-repeat");
+      break;
+    case "partly-cloudy-day":
+      $("body").css("background", "url(images/cloudy.jpg) no-repeat");
+      break;
+    case "partly-cloudy-night":
+      $("body").css("background", "url(images/cloudy_night.jpg) no-repeat");
+      break;
+    default:
+      $("body").css("background", "url(images/default.jpg) no-repeat");
   }
 }
+
